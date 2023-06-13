14d48480d892a6462ea75d0700a205efe9dfbd35
emvalencaf <edsonmvf@gmail.com>
2023-06-13 15:41:54 -0300

    create model
    created model post

---
+++ backend/src/models/index.js
@@ -0,0 +1,5 @@
+const postModel = require('./post');
+
+module.exports = {
+    postModel,
+}
---
+++ backend/src/models/post/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    prompt: { type: String, required: true },
+    photo: { type: String, required: true },
+});
+
+const PostModel = mongoose.model('Post', PostSchema);
+
+module.exports = {
+    PostModel,
+};
